a252a5cd36a781b38d861f4fce472107a2971724
Võ Đạt  Đang Học Code <166628450+vodatdammecode@users.noreply.github.com>
2024-10-01 19:53:46 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var fut = new Date("jan 12, 2023 13:29:00").getTime()
+var fut = new Date("January 29, 2025 00:00:00").getTime()
 let x = setInterval(function stime(){
     var now = new Date().getTime()
     var D = fut - now
